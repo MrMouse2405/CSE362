aaf2819d519eec0b2d964d012e7ef4e9d85ecc89
Eric Moreau <ecmpreds21@gmail.com>
2026-01-22 09:24:03 -0500

    ETest

---
+++ app/api/routes/ETest.py
@@ -0,0 +1,31 @@
+import random
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Form
+from fastapi.responses import HTMLResponse
+from pydantic.dataclasses import dataclass
+
+from app.auth import (
+    allow_admin,
+    allow_authorized,
+    allow_root,
+    allow_student,
+    allow_teacher,
+)
+
+router = APIRouter(
+    prefix="/ETest",
+    tags=["ETests"],
+)
+
+@dataclass
+class test:
+    newNum: float
+
+@router.get('/rand')
+async def rand():
+    newObject = test(newNum = 3.423)
+    return newObject
+
+
+
