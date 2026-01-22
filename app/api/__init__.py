b328a93081fb9163ff32df953e52f04137a8bf55
ericafu3 <ericafu3@gmail.com>
2026-01-22 09:26:58 -0500

    lab 3: OCdt Erica Fu Commit Attempt 1

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -7,11 +7,12 @@ Top Level API Router
 from fastapi import APIRouter
 from loguru import logger
 
-from app.api.routes import example, user
+from app.api.routes import example, user, test
 
 v0_router = APIRouter(prefix="/v0")
 v0_router.include_router(example.router)
 v0_router.include_router(user.router)
+v0_router.include_router(test.router)
 
 api_router = APIRouter(prefix="/api")
 api_router.include_router(v0_router)
